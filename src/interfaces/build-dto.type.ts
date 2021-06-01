c8a00347af5a2fcbf16fdd5f581e83133114eeab
Jacob Dharandas <jacob.dharandas@prestashop.com>
2021-06-01 19:20:24 +0200

    Added "prefix" to buildDto

--- src/interfaces/build-dto.type.ts
+++ src/interfaces/build-dto.type.ts
@@ -5,6 +5,7 @@ export type BuildDtoType = OptionalType<ConfigType, 'build'> & {
   target: any;
   auto?: {
     enabled: boolean;
+    prefix?: string;
     path?: string; // path to look for the elements in the request
     blocklist?: string[];
   };
