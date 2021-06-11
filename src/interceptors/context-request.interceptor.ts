bee304e3832e0dc68b5cdbab0d3e40532cb5c516
Jacob Dharandas <jacob.dharandas@prestashop.com>
2021-06-11 10:30:39 +0200

    Bug fixed

--- src/interceptors/context-request.interceptor.ts
+++ src/interceptors/context-request.interceptor.ts
@@ -2,6 +2,7 @@ import {
   CallHandler,
   ExecutionContext,
   Inject,
+  Injectable,
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
@@ -10,6 +11,7 @@ import { Context, getContextRequest } from '../context';
 import { CONTEXT_MODULE_CONFIG } from '../constants';
 import { ContextConfigType } from '../interfaces';
 
+@Injectable()
 export class ContextRequestInterceptor implements NestInterceptor {
   constructor(
     private readonly context: Context,
