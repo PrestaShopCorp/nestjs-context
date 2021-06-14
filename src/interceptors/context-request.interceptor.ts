f03883a9b0c2649a5cc21477aea433e217e885f8
Jacob Dharandas <jacob.dharandas@prestashop.com>
2021-06-14 17:20:23 +0200

    Removed logger from Context module to avoid problems with pino-stackdriver

--- src/interceptors/context-request.interceptor.ts
+++ src/interceptors/context-request.interceptor.ts
@@ -10,21 +10,17 @@ import { tap } from 'rxjs/operators';
 import { Context, getContextRequest } from '../context';
 import { CONTEXT_MODULE_CONFIG } from '../constants';
 import { ContextConfigType } from '../interfaces';
-import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class ContextRequestInterceptor implements NestInterceptor {
-  private logger = new Logger(ContextRequestInterceptor.name);
   constructor(
     private readonly context: Context,
     @Inject(CONTEXT_MODULE_CONFIG) private readonly config: ContextConfigType,
   ) {}
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     this.context.setRequest(getContextRequest(this.config.type, context));
-    this.logger.debug(`Context request set: ${!!this.context.request}`);
     return next.handle().pipe(
       tap(() => {
-        this.logger.debug('Clear context...');
         this.context.setRequest(null);
         this.context.clear();
       }),
