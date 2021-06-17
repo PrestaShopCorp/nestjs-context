d9e71e96abafc62cc1ebde36f7f1500d0ba25971
Jacob Dharandas <jacob.dharandas@prestashop.com>
2021-06-17 11:36:13 +0200

    Bug fixed in typing

--- src/interfaces/build-dto.type.ts
+++ src/interfaces/build-dto.type.ts
@@ -2,7 +2,7 @@ import { ContextConfigType } from './context-config.type';
 import { OptionalType } from './optional.type';
 
 export type BuildDtoType = OptionalType<ContextConfigType, 'build'> & {
-  target: any;
+  target?: any;
   auto?: {
     enabled: boolean;
     prefix?: string;
