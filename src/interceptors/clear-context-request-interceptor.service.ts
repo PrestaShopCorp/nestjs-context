0010582087f360be1213b42485391ce334d7c6da
Jacob Dharandas Méndez <jacob.dharandas@prestashop.com>
2021-07-01 09:42:45 +0200

    Bug fixed: interceptors pass after guards ! use middleware to set con… (#17)
    * Bug fixed: interceptors pass after guards ! use middleware to set context request
    
    * 0.11.1
    
    * Removed package-lock

--- src/interceptors/clear-context-request-interceptor.service.ts
+++ src/interceptors/clear-context-request-interceptor.service.ts
@@ -7,18 +7,19 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { Context, getContextRequest } from '../context';
+import { Context } from '../context';
 import { CONTEXT_MODULE_CONFIG } from '../constants';
 import { ContextConfigType } from '../interfaces';
 
 @Injectable()
-export class ContextRequestInterceptor implements NestInterceptor {
+export class ClearContextRequestInterceptor implements NestInterceptor {
   constructor(
     private readonly context: Context,
     @Inject(CONTEXT_MODULE_CONFIG) private readonly config: ContextConfigType,
   ) {}
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    this.context.setRequest(getContextRequest(this.config.type, context));
+    // @todo jdm this is not working ! check how to do it for non-http contexts :/
+    // this.context.setRequest(getContextRequest(this.config.type, context));
     return next.handle().pipe(
       tap(() => {
         this.context.setRequest(null);
