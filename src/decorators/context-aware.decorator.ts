30f7a971864e6822657c2065b4e3199f6e4c2920
Jacob Dharandas <jacob.dharandas@prestashop.com>
2021-07-10 09:22:38 +0200

    Added @ContextAware and ContextAwareMixin

---
+++ src/decorators/context-aware.decorator.ts
@@ -0,0 +1,5 @@
+import { ContextAwareMixin } from './context-aware.mixin';
+
+export const ContextAware = () => {
+  return ContextAwareMixin();
+};
--- src/decorators/correlation-id.decorator.ts
+++ src/decorators/correlation-id.decorator.ts
@@ -1,8 +1,9 @@
 import { CONTEXT_CORRELATION_ID } from '..';
-import { getObjectPropertyKeyForContext } from '../helpers';
 
 /**
- * Note: you need to add Context as DI to use this decorator
+ *  IMPORTANT: To use this decorator you need to decorate your provider with "@ContextAware"
+ *  to use this decorator
+ *
  *  Note: this decorator converts your object property into an accessor descriptor instead of
  *  data descriptor, and it will use another data descriptor as backup for the rest of sub-properties
  * @constructor
@@ -12,9 +13,7 @@ export const CorrelationId = () => (target: any, propertyKey: string) => {
     configurable: false,
     enumerable: true,
     get(): any {
-      const contextPropertyKey = getObjectPropertyKeyForContext(this);
-      const context = this[contextPropertyKey];
-      return context.get(CONTEXT_CORRELATION_ID);
+      return this.__context.get(CONTEXT_CORRELATION_ID);
     },
     set: undefined,
   });
